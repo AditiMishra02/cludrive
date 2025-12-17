24862614ab50bbf1f757db68aec8e7eef9f10662
Aditi Mishra <aditimishra589@gmail.com>
2025-12-17 14:40:29 +0000

    Initial commit - Cludrive repo (backend + frontend)

--- app/routes/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-home_bp = Blueprint('home', __name__)
-
-@home_bp.route('/', methods=['GET'])
-def home():
-    return jsonify({"message": "Welcome to Cludrive API"}), 200
